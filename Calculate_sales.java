3e0a23f387f97b61a8e80be939bf1306a538f807
Ogawa_Yuutarou <y_ogawa@plusize.co.jp>
2016-10-12 18:41:09 +0900

    second

--- Calculate_sales.java
+++ Calculate_sales.java
@@ -1,30 +1,58 @@
 package jp.co.plusize.ogawa_yuutarou;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
+
 
 public class Calculate_sales {
 	public static void main(String[] args) {
-		try {
+
+		//HashMap外に
+
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		//branch.lstが無ければエラーメッセージを出す
+
+		//fileを定義していない
+		if (!file.exists(args[0]."branch.lst")){
+			 System.out.println("支店定義ファイルが存在しません");
+		 }
+
+		 	//branch.lst内の文をカンマで区切って紐付けをする
+		 	//keyとvalueのフォーマットが違えばエラーメッセージ
+		 try {
 			File file = new File(args[0],"branch.lst");
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
-			
+
+			//一行ずつ読み込み、全ての支店コードとそれに対応する
+			//支店名を保持する。
 			String shi;
-			HashMap<String, String> map = new HashMap<String, String>();
-					while((shi = br.readLine()) != null) {
-						String[] shiten = shi.split(",", 0);
-						map.put("支店コード", shiten[0]);
-						map.put("支店名", shiten[1]);
-						System.out.println(map.entrySet());
-					}
-		br.close();
-		fr.close();
-		} catch(IOException e) {
-		System.out.println("支店定義ファイルが存在しません");
-		  }
+			while((shi = br.readLine()) != null) {
+				String[] shiten = shi.split(",", 0);
+
+
+				//エラー処理
+				//shiten[0]が3桁の数字で無ければエラーメッセージ
+				//shiten[1]がカンマ、改行が無ければエラーメッセージ
+				//どちらも通ったらmapへという流れ
+
+
+				//半角数字3桁にマッチしてる
+				//http://java-reference.sakuraweb.com/java_string_regex.html
+				if (shiten[0].matches != "^\\d{3}$") {
+					System.out.println("支店定義ファイルのフォーマットが不正です");
+
+					//配列の要素数２以外であればエラーメッセージ
+				} else if (shiten[1].length != 2) {
+					System.out.println("支店定義ファイルのフォーマットが不正です");
+				  }
+					map.put(shiten[0], shiten[1]);
+			}
+			br.close();
+			fr.close();
+		  } catch(IOException e) {
+			  }  System.out.println(map.entrySet());
+
+
 	}
 }
